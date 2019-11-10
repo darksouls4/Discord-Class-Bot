7d320f6e78bc6a4ce3f34c8600cb66d06e05666d
!            •̶๖̶̶̶ζ͜͡S₦Ø₩ ❣ <53378658+darksouls4@users.noreply.github.com>
2019-11-10 12:11:41 -0300

    Create Command.js

---
+++ src/struct/Command.js
@@ -0,0 +1,32 @@
+class Command {
+    constructor(options = {}) {
+        this.name = options.name;
+        this.aliases = options.aliases  || [];
+    }
+
+    register(client) {
+        this.client = client;
+        this.client.commands.push(this);
+    }
+    async start(message) {
+        const prefix = 's/';
+
+        const rawArgs = message.content.split(" ");
+        let names = [this.name];
+        this.aliases.forEach(alias => {
+            names.push(alias)
+        })
+
+        const find = names.map(command => prefix + command); 
+        if (find.includes(rawArgs[0])) {
+            try {
+                const args = rawArgs;
+                args.shift();
+
+                await this.run({message, args});
+            } catch (e) {}
+        }
+    }
+}
+
+module.exports = Command
