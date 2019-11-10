d51c200b8303dadba1c2a98e552a4c699d86cf4c
!            •̶๖̶̶̶ζ͜͡S₦Ø₩ ❣ <53378658+darksouls4@users.noreply.github.com>
2019-11-10 12:06:33 -0300

    Create message.js

---
+++ src/events/message.js
@@ -0,0 +1,16 @@
+const Event = require('../struct/Event')
+
+class MessageEvent extends Event {
+    constructor() {
+        super("message");
+    }
+
+    run(message) {
+        if (message.author.bot) return;
+        this.client.commands.forEach(command => {
+            if (command.start(message)) return;
+        })
+    }
+}
+
+module.exports = MessageEvent
